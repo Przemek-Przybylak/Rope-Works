4efe80a9f4d686faeb54457aac2bd4f2fc977a49
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-31 21:17:35 +0100

    implement Header component in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Container } from "./common/index";
+import { Header } from "./features/header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Container>App</Container>
     </div>
   );
