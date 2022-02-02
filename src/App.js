5b80c552ccefc039a072fc12ecbf252af8e0d77b
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-02-02 17:51:24 +0100

    added Slider in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { Container } from "./common/index";
+import { Container } from "./common/Container/index";
 import { Header } from "./features/header/Header";
+import { Slider } from "./features/slider/Slider";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Container>App</Container>
+      <Container>
+        <Slider>Slider</Slider>
+        App
+      </Container>
     </div>
   );
 }
