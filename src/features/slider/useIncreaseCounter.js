c323b64d4ea40bca7f4d6a8ba367d44a98312b88
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-02-03 15:57:33 +0100

    create useIncreaseCounter hook

---
+++ src/features/slider/useIncreaseCounter.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCounter, selectCounter, toggleCounter } from "./sliderSlice";
+
+export const useIncreaseCounter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector(selectCounter);
+
+  useEffect(() => {
+    setTimeout(() => {
+      {
+        counter < 1 ? dispatch(toggleCounter()) : dispatch(clearCounter());
+      }
+    }, 500);
+  });
+};
