ef6720977880c9b30e24a1897207b74a7e556801
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-02-06 22:04:15 +0100

    create AboutUs component

---
+++ src/features/aboutUs/AboutUs/index.js
@@ -0,0 +1,11 @@
+import { content } from "../content";
+import { Wrapper } from "./styled";
+
+export const AboutUs = () => {
+  return (
+    <>
+      <h2>O nas</h2>
+      <Wrapper>{content}</Wrapper>
+    </>
+  );
+};
