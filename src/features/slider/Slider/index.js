e9d573d3a2f2dda9dc018d8efd5c4517ae5dfa57
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-02-09 17:49:47 +0100

    added content to Slider

--- src/features/slider/Slider/index.js
+++ src/features/slider/Slider/index.js
@@ -1,8 +1,13 @@
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { selectCounter } from "../sliderSlice";
 import { useIncreaseCounter } from "../useIncreaseCounter";
-import { SliderImage, Wrapper } from "./styled";
+import {
+  ImageDescription,
+  SliderSecond,
+  SliderWrapper,
+  Wrapper,
+} from "./styled";
+import montazDabiny from "../images/montazDabiny.jpg";
 
 export const Slider = () => {
   const counter = useSelector(selectCounter);
@@ -10,7 +15,13 @@ export const Slider = () => {
   return (
     <>
       <Wrapper>
-        {counter === 0 ? <SliderImage /> : <SliderImage firstPicture />}
+        {counter === 0 ? (
+          <SliderWrapper>
+            <ImageDescription>montaż drabin</ImageDescription>
+          </SliderWrapper>
+        ) : (
+          <SliderSecond />
+        )}
       </Wrapper>
     </>
   );
