55c970a88a9ebaaaa549bfb81e281985e98273d6
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-02-09 17:49:09 +0100

    added styles for Slider component

--- src/features/slider/Slider/styled.js
+++ src/features/slider/Slider/styled.js
@@ -1,33 +1,36 @@
 import styled, { css } from "styled-components";
+import montazDabiny from "../images/montazDabiny.jpg";
 
 export const Wrapper = styled.article`
-  height: 90vh;
+  height: 92vh;
   background-color: black;
+  margin-bottom: 50px;
 `;
 
-export const SliderImage = styled.div`
-  background-color: yellow;
+export const SliderWrapper = styled.div`
+  margin: 0;
+  padding: 0;
+  background-image: url("${montazDabiny}");
   height: 100%;
-
-  ${({ firstPicture }) =>
-    firstPicture &&
-    css`
-      background-color: green;
-    `};
-
-  ${({ secondPicture }) =>
-    secondPicture &&
-    css`
-      background-color: pink;
-    `};
-
-  ${({ thirdPicture }) =>
-    thirdPicture &&
-    css`
-      background-color: brown;
-    `};
+  width: 100%;
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
 `;
+
+export const ImageDescription = styled.h3`
+  color: white;
+  font-size: 50px;
+  letter-spacing: 1.3;
+  font-weight: 900;
+  padding-left: 50px;
+  text-transform: uppercase;
+`;
+
 export const SliderSecond = styled.div`
   background-color: yellow;
   height: 100%;
+  width: 100%;
 `;
