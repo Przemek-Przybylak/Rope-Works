357991b7a2b05d1a166a307c701cc672a13b8ed8
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-02-09 17:46:27 +0100

    background color changed

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.main`
   max-width: 1350px;
-  background-color: rgba(50, 50, 50, 0.3);
+  background-color: rgba(50, 50, 50, 0.6);
   margin: 0 auto;
 `;
