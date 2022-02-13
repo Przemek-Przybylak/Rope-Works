33741f0e6860c3a06d827c08acfad0b4b29aeecc
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-02-13 20:40:00 +0100

    remove useless css import in Slider styled.js

--- src/features/slider/Slider/styled.js
+++ src/features/slider/Slider/styled.js
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import montazDabiny from "../images/montazDabiny.jpg";
 
 export const Wrapper = styled.article`
