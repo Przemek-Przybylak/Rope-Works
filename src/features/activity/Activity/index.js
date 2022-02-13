237e67349da2034fc648dbdd9e31b77a4884bca8
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-02-13 20:37:45 +0100

    added Header, added Activity posters

--- src/features/activity/Activity/index.js
+++ src/features/activity/Activity/index.js
@@ -1,15 +1,32 @@
-import { Description, Grid, Poster, Tile } from "./styled";
+import { Description, DescriptionWrapper, Grid, Poster, Tile } from "./styled";
 import activityList from "../activityList.json";
+import { Header } from "../../../common/Header";
 
 export const Activity = () => {
   return (
     <>
-      <h2> Oferta</h2>
+      <Header>Oferta</Header>
       <Grid>
         {activityList.map((activity) => (
           <Tile>
-            <Poster src={activity.poster} />
-            <Description>{activity.name}</Description>
+            {activity.id === 2 ? (
+              <Poster cleaning />
+            ) : activity.id === 3 ? (
+              <Poster washing />
+            ) : activity.id === 4 ? (
+              <Poster gwo />
+            ) : activity.id === 6 ? (
+              <Poster instalation />
+            ) : activity.id === 7 ? (
+              <Poster special />
+            ) : activity.id === 9 ? (
+              <Poster ladder />
+            ) : (
+              <Poster />
+            )}
+            <DescriptionWrapper>
+              <Description>{activity.name}</Description>
+            </DescriptionWrapper>
           </Tile>
         ))}
       </Grid>
