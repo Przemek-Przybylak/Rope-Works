92fbc4d1414d768fcef902c56054d0f99fd188dd
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-02-20 20:55:02 +0100

    added social icons, and lin ks in Social

--- src/features/header/social/Social/index.js
+++ src/features/header/social/Social/index.js
@@ -1,10 +1,15 @@
-import { Icon, IconsList } from "./styled";
+import { FacebookLogo, Icon, IconsList, LinkedInLogo } from "./styled";
 
 export const Social = () => {
   return (
     <>
       <IconsList>
-        <Icon /> <Icon /> <Icon /> <Icon />
+        <Icon href="https://www.facebook.com">
+          <FacebookLogo />{" "}
+        </Icon>{" "}
+        <Icon href="https://www.linkedin.com/in/piotr-przybylak-b8b29722b/">
+          <LinkedInLogo />{" "}
+        </Icon>
       </IconsList>
     </>
   );
