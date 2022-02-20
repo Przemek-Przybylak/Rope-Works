345a457ac05a1757ca19e5412b0eb8ad67c126fa
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-02-20 20:40:09 +0100

    create IconList and Icon styles

---
+++ src/features/header/social/Social/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const IconsList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  text-decoration: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Icon = styled.li`
+  margin: 0;
+  padding: 0;
+  width: 18px;
+  height: 18px;
+  background-color: black;
+`;
