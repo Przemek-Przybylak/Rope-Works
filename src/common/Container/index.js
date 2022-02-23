53c7359114417e00c3b5044519dfb68d699f6a45
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-02-23 15:40:39 +0100

    added background color from theme

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.main`
   max-width: 1350px;
-  background-color: rgba(50, 50, 50, 0.6);
+  background-color: ${({ theme }) => theme.colors.opacityGrey};
   margin: 0 auto;
   color: ${({ theme }) => theme.colors.white};
 `;
